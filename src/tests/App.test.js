ac896ef95e52f9c272fe80ee8308580ef14ac1a7
John <jooke98@outlook.com>
2021-08-03 15:38:33 -0600

    Moved header to app page addedrouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,3 +6,12 @@ test('renders Hello World', () => {
   const linkElement = screen.getByText(/under construction/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('Renders header items', () => {
+  render(<App />);
+  const headerTitle = screen.getByText(/I DID It/i);
+  const about = screen.getByText(/About DID/i);
+  const forum = screen.getByText(/Forum/i);
+  expect(headerTitle).toBeInTheDocument();
+  expect(forum).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+});
