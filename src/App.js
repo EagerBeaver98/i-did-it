ad5298c0d44fc5c529059d796dd229266ea8d100
John <jooke98@outlook.com>
2021-08-03 15:01:37 -0600

    Built and styled construction page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import Hello from './components/HelloWorld';
+// import Hello from './components/HelloWorld';
 import UnderConstruction from './components/UnderConstruction';
 
 
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='seperator'></div>
       <UnderConstruction />
       
     </div>
