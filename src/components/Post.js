140aaab4da9957c94136220598ef69be992c4d1c
John <jooke98@outlook.com>
2021-09-09 16:09:16 -0600

    added date

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,14 @@
+function dateFormat(utcCode) {
+  
+}
+
 function Post(props) {
+
+
   return (
     <div className="post">
       <div className="tweet">{props.post.tweet}</div>
+      <div className="time">{Date.getMonth(props.post.datetime)}</div>
     </div>
   )
 }
