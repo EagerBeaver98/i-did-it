6fb12e3576705ce8c3979a4dac63ef5a0afdbed6
John <jooke98@outlook.com>
2021-12-30 15:25:16 -0700

    css updated

--- src/components/Diary.js
+++ src/components/Diary.js
@@ -5,11 +5,11 @@ export default function Diary(props) {
   const [state, setState] = useState(diaries);
   
   return (
-    <div>
+    <div className="diary">
       <h2>Diary Entries</h2>
       {state.map((entry) => {
         return (
-          <div key={entry.id}>
+          <div className="diaryEntry" key={entry.id}>
             <p>{entry.entry}</p>
             <div>{entry.alterName}</div>
             <div>{props.dateFormat(entry.datetime)}</div>
