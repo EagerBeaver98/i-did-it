a5d09e952ae86a9b995122ea55765f3561efba16
John <jooke98@outlook.com>
2022-01-02 05:48:25 -0700

    Added text box and conditional button

--- src/components/Diary.js
+++ src/components/Diary.js
@@ -1,13 +1,28 @@
 import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { diaries } from '../data/temp data'
+import TextBox from './TextBox';
 
 export default function Diary(props) {
-  const [state, setState] = useState(diaries);
+  const [state, setState] = useState({diaries: diaries, showTextBox: false});
   
+  const textBoxHandler = () => {
+    if (state.showTextBox) {
+      return (<TextBox></TextBox>)
+    } else {
+      return (<Button id='newDiary' onClick={buttonHandler}>New Entry</Button>)
+    }
+  }
+
+  const buttonHandler = () => {
+    setState({...state, showTextBox: true})
+  }
+
   return (
     <div className="diary">
       <h2>Diary Entries</h2>
-      {state.map((entry) => {
+      {textBoxHandler()}
+      {state.diaries.map((entry) => {
         return (
           <div className="diaryEntry" key={entry.id}>
             <p>{entry.entry}</p>
---
+++ src/components/TextBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextBox() {
+  return (
+    <div>
+      <form>
+        <input type="text" name="newTextBox"></input>
+        <input type="submit" value="Post"></input>
+      </form>
+    </div>
+  )
+}
