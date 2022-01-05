2cdc4120e06a0d64d0d4c720c789949755cba209
John <jooke98@outlook.com>
2022-01-05 03:52:50 -0700

    fixed id num counter for new posts

--- src/helpers/arrayIDNumWriter.js
+++ src/helpers/arrayIDNumWriter.js
@@ -1,5 +1,10 @@
 const getNewIDNum = (arrayWithIDs) => {
-  const newID = arrayWithIDs.at(-1).id + 1;
+  var newID;
+  if (arrayWithIDs[0].id) {
+    newID = arrayWithIDs.at(-1).id + 1;
+  } else if (arrayWithIDs[0].tweetID) {
+    newID = arrayWithIDs.at(-1).tweetID + 1;
+  }
   return newID;
 };
 
